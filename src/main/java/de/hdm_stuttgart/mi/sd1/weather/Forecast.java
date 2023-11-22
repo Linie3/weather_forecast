d92adaba1717f9b8aefabb20d02f0eecd7e6f96c
iLogiq <80593205+iLogiq@users.noreply.github.com>
2023-11-22 19:11:06 +0100

    added city prompt

--- src/main/java/de/hdm_stuttgart/mi/sd1/weather/Forecast.java
+++ src/main/java/de/hdm_stuttgart/mi/sd1/weather/Forecast.java
@@ -1,6 +1,11 @@
 package de.hdm_stuttgart.mi.sd1.weather;
 
 
+import de.hdm_stuttgart.mi.sd1.weather.cities.Cities;
+import de.hdm_stuttgart.mi.sd1.weather.cities.City;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Providing terminal based weather forecast
@@ -12,6 +17,19 @@ public class Forecast {
    * @param args Yet unused.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a city: ");
+    String cityName = scanner.next();
 
+    ArrayList<City> matchingCities = new ArrayList<>();
+    for (City city : Cities.cities) {     //Going through every city element// Out of array
+      if(city.getName().contains(cityName)){
+        matchingCities.add(city);
+      }
+    }
+    System.out.println("Please choose one of the following cities: ");
+    for(int i = 0; i < matchingCities.size(); i++){
+      System.out.println((i + 1) + ": " + matchingCities.get(i).getName() + " " + matchingCities.get(i).getCountry());
+    }
   }
 }
