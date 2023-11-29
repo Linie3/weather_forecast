39e82920c3f96229812312af45476732d869c8b1
iLogiq <80593205+iLogiq@users.noreply.github.com>
2023-11-29 19:09:46 +0100

    visual_java_swing
    deine Senna ist Scheiße!

--- src/main/java/de/hdm_stuttgart/mi/sd1/weather/Forecast.java
+++ src/main/java/de/hdm_stuttgart/mi/sd1/weather/Forecast.java
@@ -1,12 +1,14 @@
 package de.hdm_stuttgart.mi.sd1.weather;
 
-
 import de.hdm_stuttgart.mi.sd1.weather.cities.Cities;
 import de.hdm_stuttgart.mi.sd1.weather.cities.City;
 import de.hdm_stuttgart.mi.sd1.weather.model.List;
 import de.hdm_stuttgart.mi.sd1.weather.model.Weather;
 import org.apache.commons.io.FileUtils;
 
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -17,8 +19,10 @@ import java.util.Scanner;
 /**
  * Providing terminal based weather forecast
  */
-public class Forecast {
+public class Forecast extends JPanel  {
 
+
+InputPanel inputPanel;
 final static String KEY = "35e9fbabc3c58c940ce6f94c3b7dd10e";
   /**
    * <p>Entry starting the application.</p>
@@ -27,21 +31,37 @@ final static String KEY = "35e9fbabc3c58c940ce6f94c3b7dd10e";
    */
   public static void main(String[] args) {
 
+    Forecast panel = new Forecast();
+    JFrame frame = new JFrame("weather_forecast");
+    frame.setSize(1100,800);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().add(panel);
+    frame.setVisible(true);
+
+
+
+
     Scanner scanner = new Scanner(System.in);
     System.out.print("Enter a city: ");
-    String cityName = scanner.next();
+    String cityName = scanner.nextLine();
+
 
     ArrayList<City> matchingCities = new ArrayList<>();
 
-
     for (City city : Cities.cities) {     //Going through every city element// Out of array
-      if (city.getName().contains(cityName)) {
+      if (city.getName().toUpperCase().contains(cityName.toUpperCase())) {
         matchingCities.add(city);
       }
+    }
+    if(matchingCities.isEmpty()){
+
+      System.err.println("Error: " +  cityName + " could not be found");
+      return;
     }
 
 
     System.out.println("Please choose one of the following cities: ");
+
     for(int i = 0; i < matchingCities.size(); i++){
 
       System.out.println((i + 1) + ": " + matchingCities.get(i).getName() + " " + matchingCities.get(i).getCountry());
@@ -59,28 +79,35 @@ final static String KEY = "35e9fbabc3c58c940ce6f94c3b7dd10e";
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-
     for (List listElement: weather.getList() ) {
       System.out.println(new Date(listElement.getDt() * 1000L));
+      System.out.println(listElement.getMain().getTemp() + " °C");
+      System.out.println(listElement.getMain().getPressure() + " Pa");
+
 
     }
-
-
-
-
-
-
   }
   public static void saveWeatherData(int id){
     String url = "https://api.openweathermap.org/data/2.5/forecast?lang=de&APPID="+ KEY +"&units=metric&id="+id;
     try {
       FileUtils.copyURLToFile(
-            new URL(url),
+              new URL(url),
               new File("./src/main/resources/weather_data.json"));
     } catch (IOException e) {
       System.out.println("Url couldn't be found");
     }
-
   }
 
+  public Forecast(){
+
+    inputPanel = new InputPanel();
+    setLayout(new BorderLayout());
+
+
+    add(inputPanel,BorderLayout.SOUTH);
+
+
+
+  }
 }
+
---
+++ src/main/java/de/hdm_stuttgart/mi/sd1/weather/InputPanel.java
@@ -0,0 +1,43 @@
+package de.hdm_stuttgart.mi.sd1.weather;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class InputPanel extends JPanel implements KeyListener {
+    JTextField cityInput;
+    JLabel inputLabel;
+    public InputPanel(){
+        cityInput = new JTextField(10);
+        inputLabel = new JLabel("Please enter your city: ");
+        Border border = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.GRAY,3, true),"Input");
+        this.setBorder(border);
+        this.add(inputLabel);
+        this.add(cityInput);
+        cityInput.addKeyListener(this);
+
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode()==10){
+            System.out.println(cityInput.getText());
+
+        }
+
+    }
+}
